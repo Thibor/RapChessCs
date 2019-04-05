d2fe17989da21d7838b8ef593888d06df898c961
Thibor <thibor@vp.pl>
2019-04-05 06:23:24 +0100

    Add files via upload

---
+++ CUci.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace RapCsChess
+{
+    class CUci
+    {
+        public string command;
+        public string[] tokens;
+
+        public int GetIndex(string key,int def)
+        {
+            for(int n = 0;n < tokens.Length;n++)
+            {
+                if (tokens[n] == key)
+                {
+                    return n + 1;
+                }
+            }
+            return def;
+        }
+
+        public int GetInt(string key,int def)
+        {
+            for (int n = 0; n < tokens.Length - 1; n++)
+            {
+                if (tokens[n] == key)
+                {
+                    return Int32.Parse(tokens[n +1]);
+                }
+            }
+            return def;
+        }
+
+        public void SetMsg(string msg)
+        {
+            tokens = msg.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            command = tokens[0];
+        }
+    }
+}
---
+++ CUndo.cs
@@ -0,0 +1,12 @@
+﻿namespace RapCsChess
+{
+    class CUndo
+    {
+        public int captured;
+        public int hash;
+        public int passing;
+        public int castle;
+        public int move50;
+        public int lastCastle;
+    }
+}
