d75137533bf0059634a3d561fb30d825f844b715
Thibor <thibor@vp.pl>
2020-06-02 16:07:43 +0100

    add CSynStop

---
+++ CSynStop.cs
@@ -0,0 +1,25 @@
+﻿namespace RapChessCs
+{
+	class CSynStop
+	{
+		private bool value;
+		private readonly object locker = new object();
+
+		public bool GetStop()
+		{
+			lock (locker)
+			{
+				return value;
+			}
+		}
+
+		public void SetStop(bool v)
+		{
+			lock (locker)
+			{
+				value = v;
+			}
+		}
+
+	}
+}
